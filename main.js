bb205039a0c8e944218832b487e5cbac96758f74
Hyebin <gpq8l8s@gmail.com>
2021-12-22 16:19:47 +0100

    added md & changed image route

--- main.js
+++ main.js
@@ -4,16 +4,65 @@
 let slideIndex = 0;
 carousel();
 
+function carousel(a, b) {
+  let i;
+  let a = document.getElementsByClassName(b);
+  for (i = 0; i < a.length; i++) {
+    a[i].Style.display = 'none';
+  }
+  slideIndex++;
+  if (slideIndex > a.length) {
+    slideIndex = 1;
+  }
+  a[slideIndex - 1].style.display = 'block';
+  setTimeout(carousel, 2000);
+}
+
+carousel(x, slider_image_studio);
+
+/*
 function carousel() {
   let i;
-  let x = document.getElementsByClassName('slider_image');
+  let x = document.getElementsByClassName('slider_image_studio');
   for (i = 0; i < x.length; i++) {
     x[i].style.display = 'none';
   }
+  slideIndex++;
+  if (slideIndex > x.length) {
+    slideIndex = 1;
+  }
   x[slideIndex - 1].style.display = 'block';
-  setTimeout(carousel, 3000);
+  setTimeout(carousel, 2000);
 }
 
+function carousel() {
+  let i;
+  let y = document.getElementsByClassName('slider_image_domicile');
+  for (i = 0; i < y.length; i++) {
+    y[i].style.display = 'none';
+  }
+  slideIndex++;
+  if (slideIndex > y.length) {
+    slideIndex = 1;
+  }
+  y[slideIndex - 1].style.display = 'block';
+  setTimeout(carousel, 2000);
+}
+
+function carousel() {
+  let i;
+  let z = document.getElementsByClassName('slider_image_jardin');
+  for (i = 0; i < z.length; i++) {
+    z[i].style.display = 'none';
+  }
+  slideIndex++;
+  if (slideIndex > z.length) {
+    slideIndex = 1;
+  }
+  z[slideIndex - 1].style.display = 'block';
+  setTimeout(carousel, 2000);
+}
+*/
 /*  Toggle Button */
 const toggleBtn = document.querySelector('.material-icons');
 const menu = document.querySelector('.menu');
---
+++ readme.md
@@ -0,0 +1,48 @@
+# welcomePhotograph
+
+---
+
+## Assignments
+
+- Necessaries
+
+  1. List of menu
+  2. Grid in css
+
+<br>
+
+- Errors on solving
+
+  1. List-style : préstation
+  2. carousel
+  3. gallery page
+
+---
+
+### Necessaries
+
+: two options has to be contained
+
+1. List of menu
+
+      <!--![](img_md/menu.png)-->
+      <img src="img_md/menu.png" width="400">
+   <br>
+
+2. Grid
+   <img src="img_md/grid.png" width="300">
+
+---
+
+### Errors on solving
+
+1. List-style : préstation
+   <img src="img_md/list-style.png" width="400">
+   : has to be same start line with photo
+   <br>
+2. Carousel
+   : dosn't work well in the page if studio
+   <br>
+3. gallery page
+   <img src="img_md/gallery.png" width="400">
+   On my local, yes but on git page photo doesn't show up in the page of gallery
